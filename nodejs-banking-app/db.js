67e9325293f9adbe83e367f9ba7161a7f0968f01
chavansourabh <sourabhchavan20.sc@gmail.com>
2022-10-18 01:52:27 +0530

    sprint 2 (manipulate data in database)

--- nodejs-banking-app/db.js
+++ nodejs-banking-app/db.js
@@ -7,10 +7,167 @@ const client = new Client({
   port: 5432,
 });
 
-client.connect((err, db) => {
+client.connect((err) => {
   if (err) {
     console.log("\n ❌ Error in Connectivity");
     return;
   }
   console.log("\n ✅Connected successfully");
 });
+
+//  ------------ TO CREATE ACCOUNT ----------------
+const createNewAccount = ({ acId, acNm, balance }) => {
+  client.query(
+    `INSERT INTO account VALUES ($1 , $2 ,$3)`,
+    [acId, acNm, balance],
+    (err, res) => {
+      if (err) console.log(`\n ❌ Problem in Creating the customer`);
+      else console.log("\n ✅ New Customer Created Succesfully");
+    }
+  );
+};
+
+// ----------- TO WITHDRAW MONEY------------------
+const withdraw = ({ acId, amount }) => {
+  client.query(
+    `select balance from account where ac_id = $1 `,
+    [acId],
+    (err, res) => {
+      if (err) {
+        console.log("\n ❌ Problem in withdrawing");
+      } else {
+        const balance = parseFloat(res.rows[0].balance);
+
+        const newBalance = balance - amount;
+        if (newBalance >= 0) {
+          client.query(
+            `update account set balance = $1 where ac_id = $2`,
+            [newBalance, acId],
+            (err, res) => {
+              if (err) console.log("\n ❌ Problem in withdrawing");
+              else console.log(`\n ✅ Amount ${amount} Withdraw succesfully`);
+            }
+          );
+        } else console.log("\n ❌ Insufficient balance");
+      }
+    }
+  );
+};
+
+// -------------- TO DEPOSITE MONEY ---------------------------
+const deposite = ({ acId, amount }) => {
+  client.query(
+    `select balance from account where ac_id = $1 `,
+    [acId],
+    (err, res) => {
+      if (err) {
+        console.log("\n ❌ Problem in Depositing");
+      } else {
+        const balance = parseFloat(res.rows[0].balance);
+
+        const newBalance = balance + amount;
+
+        client.query(
+          `update account set balance = $1 where ac_id = $2`,
+          [newBalance, acId],
+          (err, res) => {
+            if (err) console.log("\n ❌ Problem in Depositing");
+            else console.log(`\n ✅ Amount ${amount} Deposited succesfully`);
+
+            client.query(
+              `select balance from account where ac_id = $1 `,
+              [acId],
+              (err, res) => {
+                const balance = parseFloat(res.rows[0].balance);
+                console.log(`
+              \n Now Your Existing Balance is ${balance}`);
+              }
+            );
+          }
+        );
+      }
+    }
+  );
+};
+// -------------- TO TRANSFER MONEY ---------------------------
+
+const transfer = ({ srcId, destId, amount }) => {
+  // client.query(
+  //   `select balance from account where ac_id = $1`,
+  //   [srcId],
+  //   (err, res) => {
+  //     if (err) {
+  //       console.log("Enter valid id");
+  //     } else {
+  //       const srcbal = parseFloat(res.rows[0].balance);
+
+  //       client.query(
+  //         `select balance from account where ac_id = $1`,
+  //         [destId],
+  //         (err, res) => {
+  //           if (err) {
+  //             console.log("Enter valid id");
+  //           } else {
+  //             const destBal = parseFloat(res.rows[0].balance);
+
+  //             const newSrcBal = srcbal - amount;
+  //             const newDestBal = destBal + amount;
+
+  //             client.query(
+  //               `update account set balance = $1 where ac_id = $2`,
+  //               [newSrcBal, srcId],
+  //               (err, res) => {
+  //                 if (err) {
+  //                   console.log("\n ❌ Transaction failed");
+  //                 }
+  //                 console.log(
+  //                   `\n ${amount} is debited from your account number ${srcId}`
+  //                 );
+  //                 client.query(
+  //                   `update account set balance = $1 where ac_id = $2`,
+  //                   [newDestBal, destId],
+  //                   (err, res) => {
+  //                     if (err) {
+  //                       console.log("\n ❌ Transaction failed");
+  //                     } else
+  //                       console.log(
+  //                         `\n ${amount} is credited in your account number ${destId}`
+  //                       );
+  //                   }
+  //                 );
+  //               }
+  //             );
+  //           }
+  //         }
+  //       );
+  //     }
+  //   }
+  // );
+  withdraw({ acId: srcId, amount });
+  deposite({ acId: destId, amount });
+};
+
+// -------------- TO CHECK BALANCE ---------------------------
+
+const checkBalance = (acId) => {
+  client.query(
+    `select balance from account where ac_id = $1`,
+    [acId],
+    (err, res) => {
+      if (err) {
+        console.log("\n ❌ Problem in Fetching the balance");
+      } else {
+        const balance = parseFloat(res.rows[0].balance);
+        console.log(`\n Your Existing Balance is ${balance}`);
+      }
+    }
+  );
+};
+
+module.exports = {
+  createNewAccount,
+  withdraw,
+  deposite,
+  transfer,
+  checkBalance,
+};
--- nodejs-banking-app/menu.js
+++ nodejs-banking-app/menu.js
@@ -1,5 +1,12 @@
-const { resolve } = require("path");
 const readline = require("readline");
+const {
+  createNewAccount,
+  withdraw,
+  deposite,
+  transfer,
+  checkBalance,
+} = require("./db");
+
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
@@ -13,9 +20,9 @@ console.log("\n 4. Check Balance");
 console.log("\n 5. Transfer Money");
 console.log("\n 6. Exit");
 
-const ip = () => {
+const ip = (msg) => {
   return new Promise((resolve, reject) => {
-    rl.question("\n Enter Your Choice : ", (value) => {
+    rl.question(`\n  👉 ${msg}:`, (value) => {
       resolve(value);
     });
   });
@@ -23,23 +30,39 @@ const ip = () => {
 
 const start = async () => {
   while (true) {
-    const choice = await ip();
+    const choice = await ip("Enter Your Choice");
 
     if (choice == 1) {
-      console.log("Creating account");
+      console.log("Create account");
+      const acId = parseInt(await ip("Enter Your Account Id"));
+      const acNm = await ip("Enter your name");
+      const balance = 0;
+      createNewAccount({ acId, acNm, balance });
     } else if (choice == 2) {
-      console.log("Money Deposited");
+      console.log("\n ✅ Deposite Money");
+      const acId = parseInt(await ip("Enter Your Account Id"));
+      const amount = parseFloat(await ip("Enter Amount"));
+      deposite({ acId, amount });
     } else if (choice == 3) {
-      console.log("Money Withdraw");
+      console.log("\n ✅ Withdraw Money");
+      const acId = parseInt(await ip("Enter Your Account Id"));
+      const amount = parseFloat(await ip("Enter Amount"));
+      withdraw({ acId, amount });
     } else if (choice == 4) {
-      console.log("Check Balance");
+      console.log("\n ✅ Check Balance");
+      const acId = parseInt(await ip("Enter Your Account Id"));
+      checkBalance(acId);
     } else if (choice == 5) {
-      console.log("Money Transfered");
+      console.log("\n ✅ Transfer Money");
+      const srcId = parseInt(await ip("Enter Your Account Id"));
+      const destId = parseInt(await ip("Enter Destination Account Id"));
+      const amount = parseFloat(await ip("Enter Amount"));
+      transfer({ srcId, destId, amount });
     } else if (choice == 6) {
-      console.log("Exiting");
+      console.log("\n Exiting");
       process.exit();
     } else if (choice > 6) {
-      console.log("Please Enter valid number");
+      console.log("\n Please Enter valid number");
       process.exit();
     }
   }
